7f8289e27dc690587eb83497b1e7e6f3bacfca11
Aravindan12 <aravind@sparkouttech.com>
2023-11-29 17:50:54 +0530

    Defaul-src

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -13,7 +13,7 @@ class Extended extends Basic
         
         $this->addDirective(Directive::SCRIPT, 'www.google.com https://unpkg.com/');
         $this->addDirective(Directive::STYLE, 'https://fonts.googleapis.com/');
-        $this->addDirective(Directive::DEFAULT, 'https://fonts.gstatic.com/');
-        $this->addDirective(Directive::FRAME,'https://maps.google.com https://www.google.com');
+        $this->addDirective(Directive::DEFAULT, 'https://fonts.gstatic.com/ https://maps.google.com https://www.google.com');
+        // $this->addDirective(Directive::FRAME,'https://maps.google.com https://www.google.com');
     }
 }
